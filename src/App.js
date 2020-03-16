940b1c74daa636a63496b5c6c984d94dbda3c7e1
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-03-16 19:59:09 +0100

    create BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { routes } from 'routes';
+import CompaniesTable from 'components/pages/CompaniesTable';
+import CompanyDetails from 'components/pages/CompanyDetails';
 
 const App = () => (
-    <> App </>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path={routes.home} render={() => <Redirect to="/companiesData" />} />
+            <Route exact path={routes.companiesTable} component={CompaniesTable} />
+            <Route path={routes.companyDetails} component={CompanyDetails} />
+        </Switch>
+    </BrowserRouter>
   );
   
   export default App;
