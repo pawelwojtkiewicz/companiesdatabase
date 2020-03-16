322ec821dd5c6172525de9e52eeb86b97daa1b6d
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-03-16 22:06:23 +0100

    add redirect on company details after click

--- src/components/molecules/CompanyTableElement.js
+++ src/components/molecules/CompanyTableElement.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Redirect } from 'react-router-dom';
 
 const CompaniesTable = ({company}) => {
+    const [redirect, setRedirect] = useState(false);
+    const handleCompanyDetails = () => setRedirect(true);
+    
+    if(redirect) return <Redirect to={`/companiesData/details/${company.id}`} />
     return (
-        <div>
+        <div onClick={handleCompanyDetails}>
             <br />
             <div>
                 {company.id}
