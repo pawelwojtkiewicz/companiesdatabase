f7ed05db836797526a6df3e1dc4fd5b1323a75f2
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-03-16 20:07:30 +0100

    create basic CompaniesTable view

---
+++ src/components/pages/CompaniesTable.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+
+const CompaniesTable = () => {
+    
+    return (
+        <> companies table </>
+    )
+}
+
+export default CompaniesTable;
